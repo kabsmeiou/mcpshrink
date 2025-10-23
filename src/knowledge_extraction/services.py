6ca998527aa8188e8a451e77362ebb2876ee89af
kabsmeiou <cvincentcabral@gmail.com>
2025-10-23 09:49:44 +0800

    hotfix: linter errors

--- src/knowledge_extraction/services.py
+++ src/knowledge_extraction/services.py
@@ -4,7 +4,7 @@ from src.llm_client import get_groq_client, get_llm_client
 from src.knowledge_extraction.utils import format_extraction_result
 
 # how to process this by batch?
-def extract_knowledge_from_teacher(query: str, mcp_server_url: str,  mcp_server_label: str) -> dict:
+def extract_knowledge_from_teacher(query: str, mcp_server_url: str | None,  mcp_server_label: str | None) -> dict:
     """
     Extract knowledge from the given query using an LLM.
 
