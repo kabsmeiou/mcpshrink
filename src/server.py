df013d835aef0409b0ce099dbf0158f9582ebac6
kabsmeiou <cvincentcabral@gmail.com>
2025-10-24 16:35:57 +0800

    add: model tester script

--- src/server.py
+++ src/server.py
@@ -1,17 +1,18 @@
 from fastmcp import FastMCP
 
 
-def create_mcp_server():
-    mcp = FastMCP("test")
 
-    @mcp.tool
-    def add(a: int, b: int) -> int:
-        """Add two numbers"""
-        return a + b
+mcp = FastMCP("test")
 
-    @mcp.tool
-    def multiply(*, a: int, b: int) -> int:
-        """Multiply two numbers"""
-        return a * b
-    
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+@mcp.tool
+def multiply(*, a: int, b: int) -> int:
+    """Multiply two numbers"""
+    return a * b
+
+def create_mcp_server() -> FastMCP:
     return mcp
